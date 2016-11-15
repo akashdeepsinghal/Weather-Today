119f97b59a83bca29028d00e9cbd3f714ab31a29
akigupta131 <akigupta131@gmail.com>
2016-11-15 17:54:36 +0530

    Fetched JSON result successfully and printed

--- Weather Today/ViewController.swift
+++ Weather Today/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?zip=london,uk&appid=3ee8177904d514f2cc9645f283cc8416&units=metric")!
+        
+        let task = URLSession.shared.dataTask(with: url){(data, response, error) in
+            if error != nil {
+                print(error ?? "Some error fetching data from URL")
+            } else {
+                if let urlContent = data{
+                    do {
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                        print(jsonResult)
+                    } catch {
+                        print("JSON Serialization failed!")
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+        
+//        dataTask(with: url, completionHandler: (data, response, error) -> Void)
     }
 
     override func didReceiveMemoryWarning() {
