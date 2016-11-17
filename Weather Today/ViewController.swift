13d07120ae5056b0ca24bb91916d7cfa1f6ef320
akigupta131 <akigupta131@gmail.com>
2016-11-17 16:39:31 +0530

    Used Alamofire & SwiftyJSON for Data handlig

--- Weather Today/ViewController.swift
+++ Weather Today/ViewController.swift
@@ -7,33 +7,76 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var cityInput: UITextField!
+    @IBOutlet var city: UILabel!
+    @IBOutlet var tempDesc: UILabel!
+    @IBOutlet var temperature: UILabel!
+    
+    @IBAction func submit(_ sender: Any) {
+        view.endEditing(true)
+        self.hideKeyboardWhenTappedAround()
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?q="+self.cityInput.text!+"&appid=3ee8177904d514f2cc9645f283cc8416&units=metric").responseData { (resData) -> Void in
+            if((resData.result.value) != nil) {
+                let swiftyJsonVar = JSON(data : resData.result.value!)
+//                    resData.result.value!
+                print(swiftyJsonVar)
+                
+                let cityName = swiftyJsonVar["name"].rawString()
+                let weatherDesc = swiftyJsonVar["weather"][0]["description"].rawString()?.uppercased()
+                let mainTemp = swiftyJsonVar["main"]["temp"].rawString()
+//                mainTemp = mainTemp.rawString()
+               // print(cityName)
+//                print(swiftyJsonVar["weather"][0]["description"])
+//                print(swiftyJsonVar["main"]["temp"])
+        
+
+                
+                DispatchQueue.main.async {
+                    self.city.text = cityName
+                    self.tempDesc.text = weatherDesc
+                    self.temperature.text = mainTemp! + "º"
+                    self.city.alpha = 1
+                    self.tempDesc.alpha = 1
+                    self.temperature.alpha = 1
+                }
+                
+//                print(swiftyJsonVar["name"])
+//                if let resData = swiftyJsonVar["name"].arrayObject {
+//                    self.city = resData as! [[String:AnyObject]]
+//                }
+//                if self.arrRes.count > 0 {
+//                    self.tblJSON.reloadData()
+//                }
+            }
+//            print(resData.result.value!)
+//            let strOutput = String(data : resData.result.value!, encoding : String.Encoding.utf8)
+//            print(strOutput["name"])
+        }
+//        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q="+self.cityInput.text!+"&appid=3ee8177904d514f2cc9645f283cc8416&units=metric")!
+//        
+//        let task = URLSession.shared.dataTask(with: url){(data, response, error) in
+//            if error != nil {
+//                print(error ?? "Some error fetching data from URL")
+//            } else {
+//                if let urlContent = data{
+//                    let wjson = JSON(urlContent)
+//                    print(wjson)
+//                }
+//            }
+//        }
+//        
+//        task.resume()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?zip=london,uk&appid=3ee8177904d514f2cc9645f283cc8416&units=metric")!
-        
-        let task = URLSession.shared.dataTask(with: url){(data, response, error) in
-            if error != nil {
-                print(error ?? "Some error fetching data from URL")
-            } else {
-                if let urlContent = data{
-                    do {
-                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
-                        
-                        print(jsonResult)
-                    } catch {
-                        print("JSON Serialization failed!")
-                    }
-                }
-            }
-        }
-        
-        task.resume()
-        
-//        dataTask(with: url, completionHandler: (data, response, error) -> Void)
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,6 +84,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
